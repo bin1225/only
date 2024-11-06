10145640eaffc00cee8950a87d02318c51845655
kkb <gimgyubin1225@gmail.com>
2024-11-06 15:53:23 +0900

    rename: 파일명 수정 example -> project

--- src/main/java/com/project/only/OnlyApplication.java
+++ src/main/java/com/project/only/OnlyApplication.java
@@ -1,4 +1,4 @@
-package com.example.only;
+package com.project.only;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/project/only/OnlyApplicationTests.java
+++ src/test/java/com/project/only/OnlyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.only;
+package com.project.only;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
