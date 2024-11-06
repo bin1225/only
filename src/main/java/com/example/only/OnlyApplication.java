888cc04d843d28932a1cf5c1aab7887142c4ee27
kkb <gimgyubin1225@gmail.com>
2024-11-06 15:13:59 +0900

    init

---
+++ src/main/java/com/example/only/OnlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.only;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyApplication.class, args);
+	}
+
+}
