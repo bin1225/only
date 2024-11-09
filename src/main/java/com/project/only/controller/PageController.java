93bdb6b5ef0dbb7fa3fbf4c8474129bcb25f0b7b
kkb <gimgyubin1225@gmail.com>
2024-11-09 20:19:39 +0900

    feat: 페이지 저장, 조회 controller 구현 - mockMvc 사용해 테스트 구현 - title, content 클라이언트로부터 받아 일기장 페이지 생성 - 페이지 id로 페이지 조회

---
+++ src/main/java/com/project/only/controller/PageController.java
@@ -0,0 +1,31 @@
+package com.project.only.controller;
+
+import com.project.only.domain.PageRequest;
+import com.project.only.domain.PageResponse;
+import com.project.only.service.PageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class PageController {
+
+    private final PageService pageService;
+
+    @PostMapping("/only/page/add")
+    public ResponseEntity<PageResponse> addPage(@RequestBody PageRequest pageRequest) {
+        final PageResponse pageResponse = pageService.savePage(pageRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(pageResponse);
+    }
+
+    @GetMapping("/only/page/{pageId}")
+    public ResponseEntity<PageResponse> getPage(@PathVariable Long pageId) {
+        final PageResponse pageResponse = pageService.findPageById(pageId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pageResponse);
+    }
+
+}
---
+++ src/test/java/com/project/only/controller/PageControllerTest.java
@@ -0,0 +1,99 @@
+package com.project.only.controller;
+
+import com.google.gson.Gson;
+import com.project.only.domain.PageRequest;
+import com.project.only.domain.PageResponse;
+import com.project.only.service.PageService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class PageControllerTest {
+
+    @InjectMocks
+    private PageController pageController;
+    @Mock
+    private PageService pageService;
+
+    private Gson gson;
+    private MockMvc mockMvc;
+
+
+    private final String title = "title";
+    private final String content = "content";
+
+    @BeforeEach
+    void setUp() {
+        gson = new Gson();
+        mockMvc = MockMvcBuilders.standaloneSetup(pageController).build();
+    }
+
+    @Test
+    public void addPage() throws Exception {
+        //given
+        doReturn(pageResponse()).when(pageService).savePage(any(PageRequest.class));
+
+        //when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/only/page/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(pageRequest()))
+        );
+        //then
+        resultActions.andExpect(status().isCreated());
+
+        Assertions.assertThat(resultActions.andReturn().getResponse()).isNotNull();
+        final PageResponse response = gson.fromJson(resultActions.andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8),PageResponse.class);
+
+        Assertions.assertThat(response.getId()).isNotNull();
+    }
+
+    @Test void getPage() throws Exception{
+        //given
+        Long id = 1L;
+        doReturn(pageResponse()).when(pageService).findPageById(id);
+
+        //when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get("/only/page/{pageId}",id));
+
+        //then
+        resultActions.andExpect(status().isOk());
+
+        PageResponse response = gson.fromJson(resultActions.andReturn()
+                .getResponse().getContentAsString(), PageResponse.class);
+
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getTitle()).isEqualTo(title);
+        Assertions.assertThat(response.getContent()).isEqualTo(content);
+    }
+
+    private PageRequest pageRequest(){
+        return new PageRequest(title, content);
+    }
+
+    private PageResponse pageResponse(){
+        return PageResponse.builder()
+                .id(1L)
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
