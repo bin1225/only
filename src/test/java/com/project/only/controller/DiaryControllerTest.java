04e9df945197e5520e9084024f0ff8ee3eafc667
kkb <gimgyubin1225@gmail.com>
2024-11-20 23:31:32 +0900

    test: 다이어리 목록 조회 기능 테스트

--- src/test/java/com/project/only/controller/DiaryControllerTest.java
+++ src/test/java/com/project/only/controller/DiaryControllerTest.java
@@ -1,6 +1,7 @@
 package com.project.only.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.project.only.domain.Diary;
 import com.project.only.domain.DiaryRequest;
 import com.project.only.domain.DiaryResponse;
@@ -18,7 +19,10 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
@@ -63,6 +67,32 @@ public class DiaryControllerTest {
                 .getResponse()
                 .getContentAsString(StandardCharsets.UTF_8),DiaryResponse.class);
         Assertions.assertThat(diaryResponse.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("다이어리 목록 조회")
+    public void getDiaryList() throws Exception {
+        //given
+        Long memberId = 1L;
+        List<Diary> diaryList = new ArrayList<>();
+        diaryList.add(diary());
+        doReturn(diaryList).when(diaryService).getDiaries(memberId);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/only/{memberId}/diary", memberId)
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        resultActions.andExpect(status().isOk());
+
+        Type listType = new TypeToken<List<DiaryResponse>>() {}.getType();
+        List<DiaryResponse> result = gson.fromJson(resultActions.andReturn()
+                .getResponse()
+                .getContentAsString(), listType);
+
+        Assertions.assertThat(result).isNotNull();
+        Assertions.assertThat(result).isNotEmpty();
     }
 
     private Diary diary() {
